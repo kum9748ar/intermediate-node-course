14435c932d1ebcb3f6a15d5de9d254b7dde962aa
Kumar Kalyan <kum9748ar@gmail.com>
2021-12-27 13:05:05 +0530

    add user model and connect to db

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-const express= require('express');
-const mongoose= require('mongoose');
-const bodyParser= require('body-parser');
-const port=8000;
-const app= express();
-
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const port = 8000;
+const app = express();
 app.use(bodyParser.json());
+const User = require('./models/User');
+mongoose.connect('mongodb://localhost/userData');
 
-app.listen(port, ()=>{
-	console.log(`server is listening on port:${port}`)
-})
-
-// CREATE
-app.post('/users',(req,res)=>{
+// CREAT
+app.post('/users', (req, res) => {
   // User.create()
 })
 
 app.route('/users/:id')
-// READ
-.get((req,res)=>{
-  // User.findById()
-})
-// UPDATE
-.put((req,res)=>{
-  // User.findByIdAndUpdate()
-})
-// DELETE
-.delete((req,res)=>{
-  // User.findByIdAndDelete()
+  // READ
+  .get((req, res) => {
+    // User.findById()
+  })
+  // UPDATE
+  .put((req, res) => {
+    // User.findByIdAndUpdate()
+  })
+  // DELETE
+  .delete((req, res) => {
+    // User.findByIdAndDelete()
+  })
+app.listen(port, () => {
+  console.log(`server is listening on port:${port}`)
 })
